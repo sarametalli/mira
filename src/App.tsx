ae389ee29eb487286c0017e28f1560eccb64b60f
msara1111 <sara.metalli@gmail.com>
2025-10-08 17:55:58 +0200

    homepage created

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import './App.css'
 import Footer from './components/layout/Footer'
 import Navbar from './components/layout/Navbar'
+import Homepage from './components/pages/Homepage';
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-1 overflow-auto bg-neutral-100 z-0">
-        <div className='h-[4000px]'><p className='p-[10%]'>contenuto di test</p></div>
-      </main>
+      <Homepage />
       <Footer />
     </div>
   );
---
+++ src/components/pages/Homepage.tsx
@@ -0,0 +1,9 @@
+function Homepage() {
+    return(
+    <main className="flex-1 overflow-auto bg-neutral-100 z-0">
+        <div className='h-[4000px]'><p className='p-[10%]'>contenuto di test</p></div>
+      </main>
+);
+}
+
+export default Homepage;
