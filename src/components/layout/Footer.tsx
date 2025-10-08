4d10a28f25805f3d99112032ce4584678b40f546
msara1111 <sara.metalli@gmail.com>
2025-10-08 17:36:12 +0200

    footer created

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,11 +1,24 @@
-function Footer() {
-    return(
-<footer className="bg-neutral-100 fixed bottom-0 w-full py-[1%]">
-  <p className="px-[5%] text-neutral-900">+39 123456789</p>
-  <p className="px-[5%] text-neutral-900 flex items-center space-x-8"> mira@email.com</p>
-</footer>
+import { FaInstagram, FaTiktok } from "react-icons/fa";
 
-    )
+function Footer() {
+  return (
+    <footer className="bg-neutral-100 fixed bottom-0 w-full py-[1%] flex justify-between items-center px-[5%]">
+      <div className="flex flex-col text-neutral-900">
+        <p>+39 123456789</p>
+        <p>mira@email.com</p>
+      </div>
+      <div className="flex items-center space-x-6">
+        <button className="flex items-center space-x-2 text-neutral-900 hover:text-neutral-950">
+          <FaInstagram className="text-xl" />
+          <span>mira_eventi</span>
+        </button>
+        <button className="flex items-center space-x-2 text-neutral-900 hover:text-neutral-950">
+          <FaTiktok className="text-xl" />
+          <span>mira_eventi</span>
+        </button>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
