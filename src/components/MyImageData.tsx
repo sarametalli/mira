6498cfd382f940386d4bdeac89139f4cd640743f
msara1111 <sara.metalli@gmail.com>
2025-10-09 11:37:13 +0200

    add end export new images to myimagedata

--- src/components/MyImageData.tsx
+++ src/components/MyImageData.tsx
@@ -1,13 +1,23 @@
 import party from '../assets/images/party.jpg';
-import eventplanner1 from '../assets/images/eventplanner1.jpg';
-import eventplanner2 from '../assets/images/eventplanner2.jpg';
-import eventplanner3 from '../assets/images/eventplanner3.jpg';
+// import eventplanner1 from '../assets/images/eventplanner1.jpg';
+// import eventplanner2 from '../assets/images/eventplanner2.jpg';
+// import eventplanner3 from '../assets/images/eventplanner3.jpg';
 import eventplanning from '../assets/images/event_planning.jpg';
+import bride_groom from '../assets/images/bride_and_groom.jpg';
+import wedding1 from '../assets/images/enchanted_wedding1.jpg';
+import wedding2 from '../assets/images/enchanted_wedding2.jpg';
+import wedding3 from '../assets/images/enchanted_wedding3.jpg';
+import wedding4 from '../assets/images/enchanted_wedding4.jpg';
+import birtday1 from '../assets/images/great_gatsby1.jpg';
+import birtday2 from '../assets/images/great_gatsby2.jpg';
+import birtday3 from '../assets/images/great_gatsby3.jpg';
 
-const EventPlanners = [
-    eventplanner1, eventplanner2, eventplanner3
-];
+// const EventPlanners = [
+//     eventplanner1, eventplanner2, eventplanner3
+// ];
 
-export default EventPlanners;
-export { party };
-export { eventplanning };
+const Wedding = [bride_groom, wedding1, wedding2, wedding3, wedding4];
+const Birthday = [birtday1, birtday2, birtday3];
+
+
+export { eventplanning, Wedding, Birthday, party };
