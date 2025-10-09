202ae9fe8d494c7569a195fd6d660ed74c47d769
msara1111 <sara.metalli@gmail.com>
2025-10-09 11:02:46 +0200

    changed colors in about page

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -3,7 +3,7 @@ import { eventplanning } from "../MyImageData";
 function About() {
     return(
       <>
-    <div className="py-16 bg-white">  
+    <div className="py-16 bg-neutral-100">  
   <div className="container m-auto px-6 text-neutral-600 md:px-12 xl:px-6">
       <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
         <div className="md:5/12 lg:w-5/12">
