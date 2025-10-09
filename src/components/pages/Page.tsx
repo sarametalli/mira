5439f9375de63a7f1d9a9d18c34bc1800e0034e4
msara1111 <sara.metalli@gmail.com>
2025-10-09 11:50:42 +0200

    WIP page component

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -2,9 +2,10 @@
 function Page({ title, images, descriptions }: { title: string; images?: string[]; descriptions?: string[] }) {
     return (
         <div className="relative w-full h-full flex-shrink-0 bg-red-500 text-white text-3xl font-bold flex items-center justify-center">
-            {title}
-            {images}
-            {descriptions}
+            <h2 className="text-4xl mb-4">{title}</h2>
+            {images} {/* TODO mostrare le immagini */}
+            {descriptions} {/* TODO mostrare i titoli */}
+            {/* TODO creare un pattern per mostrare immagini e descrizioni*/}
           </div>
     );
 }
