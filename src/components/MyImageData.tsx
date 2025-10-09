3682dcd524a5eb55e06e82cf1ffb992f523d50f3
msara1111 <sara.metalli@gmail.com>
2025-10-09 10:55:38 +0200

    - Create about page - Add image for about page

--- src/components/MyImageData.tsx
+++ src/components/MyImageData.tsx
@@ -2,6 +2,7 @@ import party from '../assets/images/party.jpg';
 import eventplanner1 from '../assets/images/eventplanner1.jpg';
 import eventplanner2 from '../assets/images/eventplanner2.jpg';
 import eventplanner3 from '../assets/images/eventplanner3.jpg';
+import eventplanning from '../assets/images/event_planning.jpg';
 
 const EventPlanners = [
     eventplanner1, eventplanner2, eventplanner3
@@ -9,3 +10,4 @@ const EventPlanners = [
 
 export default EventPlanners;
 export { party };
+export { eventplanning };
