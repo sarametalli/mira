242fca2a4b0b32c00f7d1acbaa7e458225531006
msara1111 <sara.metalli@gmail.com>
2025-10-10 11:23:13 +0200

    update tawilnd.config for: - colors and fonts - light and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,35 @@
-/** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "media", // così sceglie automaticamente dark/light mode in base alle impostazioni del sistema operativo
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //versione light
+        background: {
+          light: "#F8F8FE",
+          dark: "#01020A",
+        },
+        text: {
+          light: "#01020A",
+          dark: "#F8F8FE",
+        },
+        imgBg: {
+          light: "#8E94F2",
+          dark: "#7c83f0",
+        },
+        primary: "#8E94F2", //primary e secondary sono uguali 
+        secondary: "#7c83f0",
+      },
+      fontFamily: {
+        //TODO
+      },
+      fontSize: {
+        //TODO
+      }
+    },
   },
   plugins: [],
 }
