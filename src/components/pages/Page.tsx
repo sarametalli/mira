563e6e686883aa2e476468602a4a686a78dc10ef
msara1111 <sara.metalli@gmail.com>
2025-10-10 21:02:11 +0200

    change font size in page and pagecontent

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -12,11 +12,17 @@ const Page: React.FC<PageProps> = ({ title, images, descriptions }) => {
 
   // immagini rimaste
   const remainingImages = images.slice(renderedImages);
-
+  const parts = title.split(" — ");
   return (
     <div className="relative cblack mb-[1%] flex flex-col items-center justify-center overflow-y-auto">
-      <h2 className="text-6xl text-center text-cblack font-calligraphy mb-[2%] p-[3%] text-brown">{title}</h2>
-      
+    <h2 className="text-6xl text-center font-calligraphy pt-[3%] text-brown">
+      {parts[0]}
+    </h2>
+    {parts[1] && (
+      <h2 className="text-2xl text-center font-titles pb-[5%]">
+        {parts[1]}
+      </h2>
+    )}
       <PageContent
         images={images}
         descriptions={descriptions}
@@ -24,7 +30,7 @@ const Page: React.FC<PageProps> = ({ title, images, descriptions }) => {
       />
 
       {remainingImages.length > 0 && (
-        <div className="mt-[3%] w-full flex flex-wrap justify-center gap-3">
+        <div className="mt-[10%] w-full flex flex-wrap justify-center gap-3">
           {remainingImages.map((img, idx) => (
         <img
           key={idx}
--- src/components/pages/PageContent.tsx
+++ src/components/pages/PageContent.tsx
@@ -12,14 +12,23 @@ const PageContent: React.FC<PageContentProps> = ({ images, descriptions, onRende
     const content = descriptions.map((desc, i) => {
       const hasFirstImage = !!images[i];
       const hasSecondImage = !!images[i + 1];
-
       if (i % 2 === 0) {
         if (hasFirstImage) count++;
         return (
           <React.Fragment key={i}>
-            <div className="flex justify-center gap-4 items-center px-[1%]">
+            <div className="flex gap-4 items-center px-[1%]">
               {hasFirstImage && (
-                <div className="relative aspect-square w-[80%] h-auto flex items-center justify-center sm:w-[50%] overflow-hidden rounded-md">
+              i === 0 ? (
+                <div className="relative w-[90%] h-auto flex items-center justify-center">
+                    <img
+                      src={images[i]}
+                      alt={`${images[i].substring(images[i].lastIndexOf("/") + 1).split(".")[0]}`}
+                      className="w-full h-full object-cover mix-blend-multiply rounded-md aspect-square"
+                    />
+                    <div className="absolute inset-0 bg-primary opacity-10 rounded-md"></div>
+                  </div>
+              ) : (
+                <div className="relative aspect-square w-[70%] h-auto flex items-center justify-center sm:w-[50%] ml-[45%] overflow-hidden rounded-md">
                   <img
                     src={images[i]}
                     alt={`${images[i].substring(images[i].lastIndexOf("/") + 1).split(".")[0]}`}
@@ -27,9 +36,11 @@ const PageContent: React.FC<PageContentProps> = ({ images, descriptions, onRende
                   />
                   <div className="absolute inset-0 bg-primary opacity-10"></div>
                 </div>
-              )}
+              )
+            )}
+
             </div>
-            <div className="flex items-center font-p justify-center text-base text-center px-[3%] text-darkbrown">
+        <div className="flex items-center font-p justify-center text-sm sm:text-base text-center px-[3%] text-darkbrown">
               {desc && <p>{desc}</p>}
             </div>
           </React.Fragment>
@@ -41,17 +52,17 @@ const PageContent: React.FC<PageContentProps> = ({ images, descriptions, onRende
 
       return (
         <React.Fragment key={i}>
-          <div className="flex items-center justify-center text-base font-p text-center px-[7%] text-darkbrown">
+        <div className="flex items-center font-p justify-center text-xs sm:text-base text-center px-[3%] text-darkbrown">
             {desc && <p>{desc}</p>}
           </div>
 
-          <div className="flex justify-center gap-4 items-center px-[1%]">
+          <div className="flex justify-center gap-4 items-center px-[1%] py-[1%]">
             {hasFirstImage && (
               <div className="relative aspect-square w-[40%] h-auto flex items-start justify-center overflow-hidden rounded-md">
                 <img
                   src={images[i]}
                   alt={`${images[i].substring(images[i].lastIndexOf("/") + 1).split(".")[0]}`}
-                  className="w-full h-full object-cover mix-blend-multiply"
+                  className="w-full h-full object-cover mix-blend-multiply rotate-15"
                 />
                 <div className="absolute inset-0 bg-primary opacity-10"></div>
               </div>
