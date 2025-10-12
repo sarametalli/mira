f13e69a0ab2a896a64b043f1842cf8424f1c7c7e
msara1111 <sara.metalli@gmail.com>
2025-10-12 16:49:08 +0200

    add gallery in homepage

--- src/components/pages/Homepage.tsx
+++ src/components/pages/Homepage.tsx
@@ -24,13 +24,17 @@ function Homepage() {
       <div className="relative max-lg:row-start-1">
         <div className="absolute inset-px rounded-lg bg-cwhite max-lg:rounded-t-4xl"></div>
         <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
-          <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-            <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Performance</p>
-            <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit maiores impedit.</p>
+          <div className="sm:px-10 sm:pt-10">
+            <p className="mt-2 text-lg font-medium tracking-tight text-cblack font-titles max-lg:text-center">Traguardi importanti</p>
+            <p className="mt-2 max-w-lg text-sm/6 text-darkgrey font-p max-lg:text-center">Che sia una laurea, un compleanno o un babyshower, il tuo sarà un evento memorabile!</p>
           </div>
-          <div className="flex flex-1 items-center justify-center px-8 max-lg:pt-10 max-lg:pb-12 sm:px-10 lg:pb-2">
-            <img src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-performance.png" alt="" className="w-full max-lg:max-w-xs" />
+  <div className="flex justify-center py-10 sm:px-10 lg:pb-4">
+    <div className="inline-flex items-center justify-center gap-2 bg-brown/20 p-2 rounded-xl">
+            <img src={homeImages[3]} alt="" className="w-full max-lg:max-w-xs aspect-square overflow-hidden rounded-md" />
+            <img src={homeImages[2]} alt="" className="w-full max-lg:max-w-xs aspect-square overflow-hidden rounded-md" />
+            <img src={homeImages[4]} alt="" className="w-full max-lg:max-w-xs aspect-square overflow-hidden rounded-md" />
           </div>
+        </div>
         </div>
         <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-black/5 max-lg:rounded-t-4xl"></div>
       </div>
