b85fd71faae1d363a405cc094c84807ee0b629d9
msara1111 <sara.metalli@gmail.com>
2025-10-12 17:02:05 +0200

    - add images - change text in homepage

--- src/components/pages/Homepage.tsx
+++ src/components/pages/Homepage.tsx
@@ -36,14 +36,14 @@ function Homepage() {
           </div>
         </div>
         </div>
-        <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-black/5 max-lg:rounded-t-4xl"></div>
+        <div className="pointer-events-none absolute inset-px rounded-lg shadow-sm outline outline-cblack/5 max-lg:rounded-t-4xl"></div>
       </div>
       <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-        <div className="absolute inset-px rounded-lg bg-white"></div>
+        <div className="absolute inset-px rounded-lg bg-cwhite"></div>
         <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(var(--radius-lg)+1px)]">
           <div className="px-8 pt-8 sm:px-10 sm:pt-10">
-            <p className="mt-2 text-lg font-medium tracking-tight text-gray-950 max-lg:text-center">Security</p>
-            <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi.</p>
+            <p className="mt-2 text-lg font-medium tracking-tight text-cblack max-lg:text-center">Cerimonie</p>
+            <p className="mt-2 max-w-lg text-sm/6 text-darkgrey max-lg:text-center">Celebra qualsiasi cerimonia con un evento speciale, fatto su misura per te!</p>
           </div>
           <div className="@container flex flex-1 items-center max-lg:py-6 lg:pb-2">
             <img src="https://tailwindcss.com/plus-assets/img/component-images/bento-03-security.png" alt="" className="h-[min(152px,40cqw)] object-cover" />
